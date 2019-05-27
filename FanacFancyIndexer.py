3fca33335963067d3c89f571b9027e9a5adade72
mlo31415 <mlo@baskerville.org>
2019-05-26 20:55:21 -0400

    Initial commit

---
+++ FanacFancyIndexer.py
@@ -0,0 +1,15 @@
+import os
+import os.path
+
+# Read the Fanac data
+fanacDataPath=r"..\FanacNameExtractor"
+with open(os.path.join(fanacDataPath, "Fanac name path pairs.txt"), "r") as f:
+    fanacReferencesText=f.read().splitlines()
+
+fancyDataPath=r"..\FancyNameExtractor"
+# Read the Fancy canonical names data
+with open(os.path.join(fancyDataPath, "Redirects.txt"), "rb") as f:
+    fancyRedirectsText=f.read().decode("cp437").splitlines()
+
+# Build a redirection table
+i=0
